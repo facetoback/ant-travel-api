5c891f3fb8078ccb4b73cbc92cbe5a2a11ff52e1
qiaoxiaod <492266367@qq.com>
2016-01-11 21:21:18 +0800

    登录接口

--- src/com/ant/web/action/AntAction.java
+++ src/com/ant/web/action/AntAction.java
@@ -2,6 +2,9 @@ package com.ant.web.action;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.ant.open.pojo.User;
+import com.ant.open.rpc.client.AntRpcClientFactory;
+import com.ant.open.rpc.client.IAntRpcService;
 import org.jboss.resteasy.annotations.Form;
 import org.jboss.resteasy.plugins.providers.multipart.InputPart;
 import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
@@ -18,7 +21,7 @@ import javax.ws.rs.*;
 public class AntAction {
 
 //    private static Log log = LogFactory.getLog(AntAction.class);
-//    private static IRealOpenRpcService realOpenRpcService = OpenRPCClientFactory.getRealOpenRpcService();
+    private static IAntRpcService antRpcService = AntRpcClientFactory.getAntRpcService();
 
     /**
      * 登录接口
@@ -32,9 +35,21 @@ public class AntAction {
     public String logon(@QueryParam("username") String username,
                         @QueryParam("password") String password){
         JSONObject json = new JSONObject();
-        json.put("statusCode", "200");
-        json.put("success", "true");
-        json.put("message", "ok");
+        try{
+            User user = antRpcService.login(username, password);
+            if(user !=null){
+                json.put("statusCode", "0000");
+                json.put("message", "ok");
+                json.put("user", user);
+            }else{
+                json.put("statusCode", "1001");
+                json.put("message", "未查询到用户信息");
+            }
+        }catch(Exception e){
+            json.put("statusCode", "1002");
+            json.put("message", "服务器异常");
+        }
+
         return json.toJSONString();
     }
 
